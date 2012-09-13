855d20614c8e42c6d0b65dd8a98086c605c5f928
Balaji D Loganathan <info@spritle.com>
2012-09-13 16:54:48 +0530

    Basic: Initial source code using rails new appname

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcommerceAppChennaiocc::Application.config.secret_token = '6f5655fdeb12197aaf948723c90c11cd2b58b447c8538a50d2fadc47f3991bf42e9fc8d3c2a9017fcd0aaf9a71beb9b827d5681192d28c12f49aac34dcf64e41'
